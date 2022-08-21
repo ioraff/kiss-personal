a2db48e5cdeaaf71f4384355ee663cf0ce979b16
Owen Rafferty <owen@owenrafferty.com>
2022-08-21 16:15:55 -0500

    chromium: fix crash w/ gcc

--- extra/chromium/files/chromium.sh
+++ extra/chromium/files/chromium.sh
@@ -3,5 +3,5 @@
 # shellcheck disable=2086
 exec /usr/lib/chromium/chromium \
     --enable-gpu-rasterization \
-    --ozone-platform-hint=auto \
+    --ozone-platform=wayland \
     $CHROME_FLAGS "$@"
